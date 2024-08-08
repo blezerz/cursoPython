6b2cd29e107df433ee77cc2407b68e67f12eb7d2
blezerz <byronignaciocerda@gmial.com>
2024-08-08 08:11:54 -0400

    cuarto comit

--- 06-dia6/8-Proyecto/recetario.py
+++ 06-dia6/8-Proyecto/recetario.py
@@ -1,24 +1,73 @@
-def menuInicial():
-    primeraOpcion=input("""Escoje una opcion
-1)Leer una Receta
-2)Cargar nueva Receta
-3)Crear nueva Categoria
-4)Eliminar Receta
-5)Elimnar Categoria
-6)Finalizar Programa
-""")
-    return primeraOpcion
+import os
+from pathlib import Path
+from os import system
 
-menuUno=9
-while True:
-    try:
-        menuUno=int(menuInicial())
-        if menuUno <=1 and menuUno >=6:
-            print('correcto')
-            break
-    except:
-        print('ingrese dato correcto')
+mi_ruta= Path(Path.home(),'Recetas')
 
+def contar_recetas(ruta):
+    contador = 0
+    for txt in Path(ruta).glob("**/*.txt"):
+        contador += 1
 
-print(menuUno)
+    return contador
+
     
+def inicio():
+    system('cls')
+    print('*' * 50)
+    print("Bienvenido al administrador de recetas")
+    print('*' * 50)
+    print('\n')
+    print(f"Las Recetas se encuentran en {mi_ruta}")
+    print(f"Total Recetas:{contar_recetas(mi_ruta)}")
+
+    eleccion_menu= 'x'
+    while not eleccion_menu.isnumeric() or int(eleccion_menu) not in range(1,7):
+        print('Elige una opcion:')
+        print('''
+[1]Leer Receta
+[2]Crear receta nueva
+[3]Crear categoria nueva
+[4]Eliminar Recerta
+[5]Eliminar categoria
+[6]Salir del progarama''')
+eleccion_menu = input()
+
+inicio()
+
+menu = 0 
+if menu == 1:
+    #mostrar Categorias
+    #Elegir categoria
+    #mostrar Recetas
+    #elegir Receta
+    #leer receta
+    #volver al inicio
+    pass
+elif menu == 2:
+    #mostrar Categoria
+    #Elegir categoria
+    #crear Receta nueva
+    #volver a incio
+    pass
+elif menu==3:
+    #crear categoria
+    #volver inicio
+    pass
+elif menu ==4:
+    #mostrar Categorias
+    #Elegir categoria
+    #mostrar Recetas
+    #elegir Receta
+    #Eliminar Receta receta
+    #volver al inicio
+    pass
+elif menu == 5:
+    #mostrar categoria
+    #elegir categoria
+    #eliminar categoria
+    #volver inicio
+    pass
+elif menu ==6:
+    #finalizar programa
+    pass
